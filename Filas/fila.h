b4b30fe4f9ada9b5d284bb26dc84bcb7ccfbfbe4
Daniel Luca <daniel_luca.2014@outlook.com>
2022-10-15 20:19:19 -0300

    Update fila.h

--- Filas/fila.h
+++ Filas/fila.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define avanca(i) (i = (i+1) % F->max)
 
 typedef char Itemf;
 
@@ -27,6 +28,27 @@ int vaziaf(Fila F) {
 
 int cheiaf(Fila F) {
     return ( F->total == F->max);
+}
+
+void enfileira(Itemf x, Fila F) {
+    if ( cheiaf(F)) { puts("fila cheia!"); abort(); }
+    F->item[F->final] = x;
+    avanca(F->final);
+    F->total++;
+}
+
+Itemf desenfileira(Fila F) {
+    if ( vaziaf(F) ) { puts("fila vazia!"); abort(); }
+    Itemf x = F->item[F->inicio];
+	avanca(F->inicio);
+	F->total--;
+    return x;
+}
+
+void destroif(Fila *G) {
+    free((*G)->item);
+    free(*G);
+    *G = NULL;
 }
 
 
